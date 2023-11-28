5f70ea11b4d24f4bf21fef19bca4d84c16fdc2ad
Marc Albrecht <marc.albrecht@mailbox.tu-dresden.de>
2023-11-28 17:47:29 +0100

    Updated plot script and image

--- testdata/main.py
+++ testdata/main.py
@@ -15,16 +15,19 @@ def main():
     timestamps = [int(d[2][1:]) for d in data]
     tmin, tmax = timestamps[0], timestamps[-1]
     print("Timestamp bounds:", tmin, tmax)
+    timestamps = [(t - tmin) / 1e9 for t in timestamps]
+    tmin, tmax = timestamps[0], timestamps[-1]
 
     methods = [d[5] for d in data]
     print("Most called methods:", Counter(methods).most_common(25))
 
-    distance = [(timestamps[i + 1] - timestamps[i]) / 1000 for i in range(len(timestamps) - 1)]
-    print("10 smallest distances:", heapq.nsmallest(10, distance))
-    print("Distances under 15us:", len([d for d in distance if d <= 15]))
-    print("Distances between 15 and 50us:", len([d for d in distance if 50 >= d > 15]))
-    print("Distances between 50 and 200us:", len([d for d in distance if 200 >= d > 50]))
-    print("Distances greater than 200us:", len([d for d in distance if d > 200]))
+    distance = [(timestamps[i + 1] - timestamps[i]) for i in range(len(timestamps) - 1)]
+    print("10 smallest distances:", [f"{t * 1e6:.2F}us" for t in heapq.nsmallest(10, distance)])
+    print("Distances under 15us:", len([d for d in distance if d <= 15e-6]))
+    print("Distances between 15 and 50us:", len([d for d in distance if 50e-6 >= d > 15e-6]))
+    print("Distances between 50 and 200us:", len([d for d in distance if 200e-6 >= d > 50e-6]))
+    print("Distances greater than 200us:", len([d for d in distance if d > 200e-6]))
+    print("Distances greater than 1ms:", len([d for d in distance if d > 1e-3]))
 
     """
     addresses = [(int(d[3], 16), int(d[4][:-1], 10), d[5]) for d in data]
@@ -43,27 +46,34 @@ def main():
     print("Total overlaps:", count)
     """
 
-    timestamps = [(t - tmin) / (tmax - tmin) for t in timestamps]
+    # timestamps = [(t - tmin) / (tmax - tmin) for t in timestamps]
 
-    fig, ax = plt.subplots(2)
+    fig, ax = plt.subplots(3, figsize=(10, 10), dpi=300)
 
     density = gaussian_kde(timestamps)
     density.set_bandwidth(0.01)
-    xs = np.linspace(0, 1, 500)
+    xs = np.linspace(tmin, tmax, 500)
     ax[0].plot(xs, density(xs))
     ax[0].set_title('Event occurrences')
 
-    distance = np.log10(distance)
-    density = gaussian_kde(distance)
+    tdistance = np.log10(distance)
+
+    xs = timestamps[1:]
+    ax[1].scatter(xs, distance, c=tdistance, s=0.3, cmap="coolwarm")
+    ax[1].set_title('Time between events')
+    ax[1].set_yscale('log')
+
+    density = gaussian_kde(tdistance)
     density.set_bandwidth(0.01)
 
-    xs = np.linspace(min(distance), max(distance), 500)
-    ax[1].plot(xs, density(xs))
-    ax[1].xaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0E}'.format(10**y)))
-    ax[1].set_title('Time delta between events')
+    xs = np.linspace(min(tdistance), max(tdistance), 500)
+    ax[2].plot(xs, density(xs))
+    ax[2].xaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0E}'.format(10**y)))
+    ax[2].set_title('Distribution of time between events')
 
     fig.tight_layout()
     plt.show()
+    # plt.savefig("plot.svg", dpi=10)
 
 
 def overlaps(x: tuple, y: tuple) -> bool:
