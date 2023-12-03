5a66c5104c65364f9fa6ab1f88e09b77d622f582
Marc Albrecht <marc.albrecht@mailbox.tu-dresden.de>
2023-12-03 16:39:06 +0100

    Added energy benchmark scripts

--- script/run.sh
+++ script/run.sh
@@ -5,6 +5,7 @@ handlers=("dbus" "fifo" "queue" "dgram" "stream" "udp" "tcp" "memory" "mapped" "
 
 cpu_reader=0
 cpu_writer=1
+cpu_writer_dual=18
 
 logs=./logs/$(date +%F_%H-%M-%S)
 mkdir -p "$logs"
@@ -15,10 +16,19 @@ iterations=1000
 delay=10
 
 for handler in "${handlers[@]}"; do
-  echo "> Running $handler"
+  echo "> Running $handler (single socket)"
 
   taskset -c "$cpu_reader" "$program" "latency" "$handler" "reader" "$iterations" "$delay" >> "$logs/latency_$handler""_reader.log" 2>&1 &
   sleep 0.2 && taskset -c "$cpu_writer" "$program" "latency" "$handler" "writer" "$iterations" "$delay" >> "$logs/latency_$handler""_writer.log" 2>&1 &
+
+  wait && sleep 1
+done
+
+for handler in "${handlers[@]}"; do
+  echo "> Running $handler (dual socket)"
+
+  taskset -c "$cpu_reader" "$program" "latency" "$handler" "reader" "$iterations" "$delay" >> "$logs/latency_$handler""_reader.log" 2>&1 &
+  sleep 0.2 && taskset -c "$cpu_writer_dual" "$program" "latency" "$handler" "writer" "$iterations" "$delay" >> "$logs/latency_$handler""_writer.log" 2>&1 &
 
   wait && sleep 1
 done
@@ -51,23 +61,55 @@ for handler in "${handlers[@]}"; do
 
     taskset -c "$cpu_reader" "$program" "execution" "$handler" "reader" "$iterations" "$size" "$delay" >> "$logs/execution_$handler""_reader.log" 2>&1 &
     sleep 0.2 && taskset -c "$cpu_writer" "$program" "execution" "$handler" "writer" "$iterations" "$size" "$delay" >> "$logs/execution_$handler""_writer.log" 2>&1 &
-  
+
     wait && sleep 1
   done
 done
 
 echo "Running real world benchmark"
 
-path=./testdata/fifo_trace.txt
+paths=("./testdata/fifo_trace.txt")
 threshold=10
 
 for handler in "${handlers[@]}"; do
-  echo "> Running $handler"
+  for path in "${paths[@]}"; do
+    echo "> Running $handler with $path"
 
-  taskset -c "$cpu_reader" "$program" "realworld" "$handler" "reader" "$path" "$threshold" >> "$logs/realworld_$handler""_reader.log" 2>&1 &
-  sleep 0.2 && taskset -c "$cpu_writer" "$program" "realworld" "$handler" "writer" "$path" "$threshold" >> "$logs/realworld_$handler""_writer.log" 2>&1 &
+    taskset -c "$cpu_reader" "$program" "realworld" "$handler" "reader" "$path" "$threshold" >> "$logs/realworld_$handler""_reader.log" 2>&1 &
+    sleep 0.2 && taskset -c "$cpu_writer" "$program" "realworld" "$handler" "writer" "$path" "$threshold" >> "$logs/realworld_$handler""_writer.log" 2>&1 &
 
-  wait && sleep 1
+    wait && sleep 1
+  done
+done
+
+echo "Running reduced energy benchmark"
+
+iterations=10000
+delay=10
+
+for handler in "${handlers[@]}"; do
+  echo "> Running $handler with reduced data"
+
+  metricq-summary -d -- bash -c "taskset -c $cpu_reader timeout 60 $program latency $handler reader $iterations $delay >> /dev/null &
+    sleep 0.2 && taskset -c $cpu_writer timeout 59 $program latency $handler writer $iterations $delay >> /dev/null &
+    wait" >> "$logs/energy_$handler""_reduced.log" 2>&1
+
+  sleep 1
+done
+
+echo "Running full energy benchmark"
+
+iterations=1000000000
+size=128
+
+for handler in "${handlers[@]}"; do
+  echo "> Running $handler with many data"
+
+  metricq-summary -d -- bash -c "taskset -c $cpu_reader timeout 60 $program throughput $handler reader $iterations $size >> /dev/null &
+    sleep 0.2 && taskset -c $cpu_writer timeout 59 $program throughput $handler writer $iterations $size >> /dev/null &
+    wait" >> "$logs/energy_$handler""_full.log" 2>&1
+
+  sleep 1
 done
 
 echo "Finished benchmarks"
